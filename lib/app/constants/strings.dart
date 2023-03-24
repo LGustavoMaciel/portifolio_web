1bec2b9fb43b34c104c17e772e4c92c348b8dfab
Luis Gustavo S Maciel <gfromwork011@gmail.com>
2023-03-24 01:15:58 -0300

    Inicio do portifolio

---
+++ lib/app/constants/strings.dart
@@ -0,0 +1,20 @@
+class Strings {
+  Strings._();
+
+  static const String about_me = 'Sobre mim';
+  static const String about = 'Sobre';
+  static const String me = ' Mim';
+  static const String portfolio = 'Portfoli';
+  static const String o = 'O';
+  static const String headline = 'Olá Meu nome é Luis Gustavo, sou Desenvolvedor Front-end e Mobile';
+  static const String summary =
+      'Atualmente trabalho utilizando Dart e Flutter, para desenvolvimento multi-plataforma, porém com foco em Mobile. Em estudo de outras linguagens para expandir minha base de conhecimento, desejando um dia atuar não só no back-end mais também como Fullstack.';
+  static const String experience = 'Historico Proficional';
+  static const String skills_i_have = 'Habilidades e Experiencias:';
+  static const String rights_reserved = '©2023 Luis Gustavo Sousa Maciel';
+
+  // menu items
+  static const String menu_home = 'Inicio';
+  static const String menu_about = 'Sobre';
+  static const String menu_contact = 'Contato';
+}
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:portfolio_web/app/ui/home_page.dart';
+import 'package:portfolio_web/app/utils/screen/screen_utils.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(brightness: Brightness.light),
+      home: MyAppChild(),
+    );
+  }
+}
+
+class MyAppChild extends StatefulWidget {
+  @override
+  _MyAppChildState createState() => _MyAppChildState();
+}
+
+class _MyAppChildState extends State<MyAppChild> {
+  @override
+  Widget build(BuildContext context) {
+    ScreenUtil.instance = ScreenUtil.getInstance()..init(context);
+    return HomePage();
+  }
+}
